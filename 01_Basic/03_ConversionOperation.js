8e997cffc96dab1e3e73058464b0180ab54c9a30
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-03-29 14:06:05 +0000

    conversion and operation

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,30 @@
+let score =null;
+//console.log(typeof(score));
+let ValNumber = Number(score)
+//console.log(typeof(ValNumber));
+//console.log(ValNumber);
+
+// "33" => 33
+// "33abc" => NAN
+
+let a = 0
+let b = Boolean(a)
+//console.log(b);
+
+//""=> false
+//"Aaryan" => true
+
+
+//************************************ OPERATION **********************************************
+
+
+let value = 3;
+let negValue = -value; //  convert positive value in negative value
+// console.log(negValue);
+// console.log(1+2+'4');
+// console.log(+true);//'+' use to print in number value ex.true == 1;
+let age = 16;
+age++
+console.log(age);
+
+
