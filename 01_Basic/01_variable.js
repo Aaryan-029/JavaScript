65b78511ca1109b0256e2a0ff2a8642f2b6aaa13
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-03-29 13:26:53 +0000

    container config done

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,5 @@
+const accountId = 144567
+let accountEmail = "aaryanbhandari@gmail.com"
+var accountPassword = "123456"
+accountCity = "Surat"
+console.table([accountEmail,accountId,accountPassword,accountCity])
---
+++ 01_Basic/02_DataType.js
@@ -0,0 +1,5 @@
+"use strict";//treat all js code as newer version
+// alert("hello")// we are using nodejs not browser
+console.log(3+3);
+console.log(typeof 4);//typeof used to find datatype of variable or values
+
