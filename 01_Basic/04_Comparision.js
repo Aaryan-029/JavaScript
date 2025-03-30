16367a67a15f7ef533cc83ff78b84eea87e497a5
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-03-30 12:26:48 +0000

    comparision

---
+++ 01_Basic/04_Comparision.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+// console.log(2 > 1);
+
+// avoid them
+// console.log(null==0);// false
+// console.log(null>=0); // >= is convert null in 0(True)
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0); 
+
+//=== check stricly
+console.log(2 == "2");
+
