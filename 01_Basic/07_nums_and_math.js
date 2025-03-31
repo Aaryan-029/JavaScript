521c504980322b3d48d41652b325fb2c20ac9198
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-03-31 13:58:55 +0000

    number and maths

--- 01_Basic/07_nums_and_math.js
+++ 01_Basic/07_nums_and_math.js
@@ -1,29 +1,29 @@
-// const score = 400;
+const score = 400;
 
-// const balance = new Number(100);
-// console.log(balance);
+const balance = new Number(100);
+console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(typeof balance);
+console.log(balance.toString().length);
+console.log(typeof balance);
 
-// console.log(balance.toFixed(3))
+console.log(balance.toFixed(3))
 
-// const otherNum = 123.4667;
-// console.log(otherNum.toPrecision(6));
+const otherNum = 123.4667;
+console.log(otherNum.toPrecision(6));
 
-// const hun = 1000000
-// console.log(hun.toLocaleString('en-IN'));
+const hun = 1000000
+console.log(hun.toLocaleString('en-IN'));
 
 //******************************** MATHS **************************************** */
 
-// console.log(Math.PI);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.8));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.sqrt(16));
-// console.log(Math.max(2,7,2,4,56));
-// console.log(Math.min(2,7,2,4,56));
+console.log(Math.PI);
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.sqrt(16));
+console.log(Math.max(2,7,2,4,56));
+console.log(Math.min(2,7,2,4,56));
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
 
