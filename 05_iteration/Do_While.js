34cfc0622f6d3f98477c9ef9e8186fe876b5d116
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-05-27 14:18:13 +0000

    Do_While loop information

---
+++ 05_iteration/Do_While.js
@@ -0,0 +1,15 @@
+let index = 0
+while (index <=10){
+    console.log(`value of i = ${index}`);
+    index+=2
+    
+}
+
+//do while
+
+let score = 1
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while(score<=10)
