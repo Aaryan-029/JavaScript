ba0e15be95a8abed36422564440f75ebf88cfe4f
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-05-27 14:17:49 +0000

    Basic loop information

---
+++ 05_iteration/one.js
@@ -0,0 +1,35 @@
+// for loop
+
+// for (let index = 0; index < 11; index++) {
+//     const element =index;
+//     console.log(element);
+// }
+// const array = ["aaryan",'ramesh','csk']
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
+// break and continue
+
+// for (let index = 0; index < 20; index++) {
+//     if(index === 5){
+//         console.log("Detected 5");
+//         break;
+//     }
+    
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
+// for (let index = 0; index < 20; index++) {
+//   if(index === 5){
+//         console.log("Detected 5");
+//         continue;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
