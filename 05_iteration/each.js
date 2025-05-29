3280e66acb1efb03b77c5740d1272ca2ca742268
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-05-29 14:07:29 +0000

    for each loop

---
+++ 05_iteration/each.js
@@ -0,0 +1,42 @@
+const coding = ['js','python','java']
+coding.forEach( function (item){
+    //console.log(item);
+    
+})
+
+coding.forEach( (item) => {
+   // console.log(item);
+    
+})
+
+// function callme(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(callme)
+
+// coding.forEach((item,index,arr) => {
+// console.log(item,index,arr);
+
+// })
+
+const myCoding = [
+    {
+        name:'javascript',
+        file:'js'
+    },
+    {
+        name:'java',
+        file:'java'
+    },
+    {
+        name:'python',
+        file:'py'
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.name);
+    
+})
