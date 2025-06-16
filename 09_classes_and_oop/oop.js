a88bcbc39e15f0b5cbb0cbda017c0f91e761b030
Aaryan-029 <aaryanbhandari848@gmail.com>
2025-06-16 10:57:55 +0000

    Basiv OOP info

---
+++ 09_classes_and_oop/oop.js
@@ -0,0 +1,25 @@
+const user = {
+    username:'Aaryan',
+    LoginCount:7,
+    signedIn:true,
+    getUserDetails:function(){
+        //console.log('user details from database');
+        //console.log(`username:${this.username}`);
+        //console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+function User(userName,loginCount,isLoggedIn){
+    this.userName = userName
+    this.LoginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+    //return this
+}
+
+const userOne = new User('Aaryan',12,true)
+const userTwo = new User('js',8,false)
+console.log(userOne);
+console.log(userTwo);
